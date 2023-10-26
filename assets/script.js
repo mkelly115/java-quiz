842326f8b748742135a61d84a63a1c805b8905a0
michael <mikek222@gmail.com>
2023-10-26 15:25:09 -0400

    cleaned up HTML, re-positioned buttons on landing page, added initial questions array for JS

---
+++ assets/script.js
@@ -0,0 +1,30 @@
+let questions = [
+    {
+        question: "What is the starting position of an array?",
+        answer: "0",
+        options: ["0","1","2","3"]
+    },
+    {
+        question: "An array is stored by using which of the following notation:",
+        answer: "[]",
+        options: ["{}","()","[]","<>"]
+
+    },
+    {
+        question: "What does DOM stand for?",
+        answer: "Document Object Model",
+        options: ["Document Object Magnification","Dynamic Object Model","Delisting Orator qModel","Document Object Model"]
+    },
+    {
+        question: "Who invented JavaScript?",
+        answer: "Brendan Eich",
+        options: ["John Schmidt","Brendan Eich","Richard Dragon","Chris Tefaux"]
+    },
+    {
+        question: "What year was JavaScript developed?",
+        answer: "1995",
+        options: ["1993","1991","1995","1999"]
+    }
+]
+
+
