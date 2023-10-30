4520d2c039ac28cba35ac055db96169b2f67ae57
michael <mikek222@gmail.com>
2023-10-30 18:07:51 -0400

    timer now counts down from 60

--- assets/script.js
+++ assets/script.js
@@ -26,6 +26,37 @@ let prompts = [
         options: ["1993","1991","1995","1999"]
     }
 ]
+function initializeTimer(){
+const timeEl = document.querySelector(".timer")
+let count = 60;
+timeEl.innerHTML=count;
+setInterval(function() {
+      count--;
+      timeEl.innerHTML=count;
+      if (count === 0) {
+        clearInterval(timer);
+      }
+    }, 1000);
+//  const timer = setInterval(function() {
+//   count--;
+//   if (count === 0) {
+//     clearInterval(timer);
+//   }
+// }, 1000);
+
+}
+
+function initializeQuiz(){
+    const startBtnEl = document.querySelector(".start-quiz")
+    console.log(startBtnEl)
+    startBtnEl.addEventListener("click", function(event) {
+       console.log (event)
+       initializeTimer()
+      });     
+}
+
+
+initializeQuiz()
 // I HAVE ZERO CLUE IF THIS WORKS CAUSE NOTHING ELSE DOES
 // let count = 75;
 // const timer = setInterval(function() {
@@ -35,42 +66,42 @@ let prompts = [
 //   }
 // }, 1000);
 
-let questionsEl = document.querySelector("#question")
-let answerEl = document.querySelector("answer")
-let startBTN = document.querySelector("#start")
-let timeEL = document.querySelector("#time")
-let resultEL = document.querySelector("#results")
+// let questionsEl = document.querySelector("#question")
+// let answerEl = document.querySelector("answer")
+// let startBTN = document.querySelector("#start")
+// let timeEL = document.querySelector("#time")
+// let resultEL = document.querySelector("#results")
 
-let Score = 0
-let currentQuestion = 0
+// let Score = 0
+// let currentQuestion = 0
 
-function showQuestion () {
+// function showQuestion () {
 
-    const questionData = prompts [questionsEl];
+//     const questionData = prompts [questionsEl];
 
-    let questionElement = document.createElement("div");
-    questionElement.className = "question";
-    questionElement.innerHTML = questionData;
+//     const questionElement = document.createElement("div");
+//     questionElement.className = "question";
+//     questionElement.innerHTML = questionData.questionElement;
 
- for (let i = 0; i < question.length; i++) {
-    text += prompts[i]
+//  for (let i = 0; i < prompts.length; i++) {
+//     text += prompts[i]
   
-    const radio = document.createElement("input");
-    radio.type = "radio";
-    radio.name = "quiz"
-    radio.value = "answer";
+//     const radio = document.createElement("input");
+//     radio.type = "radio";
+//     radio.name = "quiz"
+//     radio.value = "answer";
 
-    option.appendChild(radio);
-    option.appendChild(optionText);
-    optionsElement.appendChild(option);
-}
+//     option.appendChild(radio);
+//     option.appendChild(optionText);
+//     optionsElement.appendChild(option);
+// }
 
-  quizContainer.innerHTML = '';
-  quizContainer.appendChild(questionElement);
-  quizContainer.appendChild(optionsElement);
-}
+//   quizContainer.innerHTML = '';
+//   quizContainer.appendChild(questionElement);
+//   quizContainer.appendChild(optionsElement);
+// }
 
-showQuestion()
+// showQuestion()
 
 
 
