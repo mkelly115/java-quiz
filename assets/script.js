6e10f999f53287fbf4530dea83164bff2c255ea5
michael <mikek222@gmail.com>
2023-10-30 17:06:10 -0400

    reworked HTML to allowed timer and buttons, cleaned up CSS

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-let questions = [
+let prompts = [
     {
         question: "What is the starting position of an array?",
         answer: "0",
@@ -26,5 +26,51 @@ let questions = [
         options: ["1993","1991","1995","1999"]
     }
 ]
+// I HAVE ZERO CLUE IF THIS WORKS CAUSE NOTHING ELSE DOES
+// let count = 75;
+// const timer = setInterval(function() {
+//   count--;
+//   if (count === 0) {
+//     clearInterval(timer);
+//   }
+// }, 1000);
+
+let questionsEl = document.querySelector("#question")
+let answerEl = document.querySelector("answer")
+let startBTN = document.querySelector("#start")
+let timeEL = document.querySelector("#time")
+let resultEL = document.querySelector("#results")
+
+let Score = 0
+let currentQuestion = 0
+
+function showQuestion () {
+
+    const questionData = prompts [questionsEl];
+
+    let questionElement = document.createElement("div");
+    questionElement.className = "question";
+    questionElement.innerHTML = questionData;
+
+ for (let i = 0; i < question.length; i++) {
+    text += prompts[i]
+  
+    const radio = document.createElement("input");
+    radio.type = "radio";
+    radio.name = "quiz"
+    radio.value = "answer";
+
+    option.appendChild(radio);
+    option.appendChild(optionText);
+    optionsElement.appendChild(option);
+}
+
+  quizContainer.innerHTML = '';
+  quizContainer.appendChild(questionElement);
+  quizContainer.appendChild(optionsElement);
+}
+
+showQuestion()
+
 
 
