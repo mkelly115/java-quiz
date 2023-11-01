f4108602e20f1a7fb40004a00bf743274b990ef0
michael <mikek222@gmail.com>
2023-11-01 14:47:24 -0400

    CSS reformatted and cleaned for planned additions, got initial start page to render with space for expected functions

--- assets/script.js
+++ assets/script.js
@@ -48,21 +48,136 @@ const metadata = {
 //     }, 1000);
 // }
 
+  
+  function initCountDown() {
+    const timeEl = document.querySelector(".timer");
+    const timer = setInterval(function() {
+      metadata.timeLeft--;
+      timeEl.innerHTML = `Time Left: ${metadata.timeLeft}`;
+      if (metadata.timeLeft <= 0) {
+        timeEl.innerHTML = "Time Left: 0";
+        clearInterval(timer);
+        renderQuizApp();
+      }
+      if (metadata.questionsAnswered === 5) {
+        timeEl.innerHtml = `Time Left: ${metadata.timeLeft}`;
+        clearInterval(timer);
+        renderQuizApp();
+      }
+    }, 1000);
+  };
+
+  
+function generateStartPageTemplate() {
+    const quizContainer = document.querySelector(".quiz-container");
+    const startPageContainer = document.createElement("div");
+    startPageContainer.classList = "flex-centered start-page";
+  
+    quizContainer.appendChild(startPageContainer);
+    startPageContainer.innerHTML = `
+      <h1>Coding Quiz Challenge</h1>
+      <button type="button" class="button start-quiz">Start Quiz</button>
+      <button type="button" class="button view-hi-scores">View Hi-Scores</button>
+    `;
+  };
+
+  function renderQuizApp() {
+    if (!metadata.quizStarted) {
+      generateStartPageTemplate();
+    }}
+
 function handleQuizStarted() {
-    const $startBtnEl = document.querySelector(".start-quiz");
-    const $viewHiScoresBtnEl = document.querySelector(".view-hi-scores");
-    $startBtnEl.addEventListener("click", function(e) {
+    const startBtnEl = document.querySelector(".start-quiz");
+    const viewHiScoresBtnEl = document.querySelector(".view-hi-scores");
+    startBtnEl.addEventListener("click", function(e) {
       metadata.quizStarted = true;
       renderQuizApp();
       initCountDown();
     });
-    $viewHiScoresBtnEl.addEventListener("click", function(e) {
-      handleViewHiScores();
-    });
+    // viewHiScoresBtnEl.addEventListener("click", function(e) {
+    //   handleViewHiScores();
+    // });
+  };
+
+function generateQuestionPageTemplate() {
+    const quizContainer = document.querySelector(".quiz-container");
+    const questionPageContainer = document.createElement("div");
+    questionPageContainer.className = "flex-centered";
+    const currentQuestion = QUIZ_STORE[metadata.currentQuestion].question;
+    const answerA = QUIZ_STORE[metadata.currentQuestion].options[0];
+    const answerB = QUIZ_STORE[metadata.currentQuestion].options[1];
+    const answerC = QUIZ_STORE[metadata.currentQuestion].options[2];
+    const answerD = QUIZ_STORE[metadata.currentQuestion].options[3];
+
+    emptyPreviousPageContent(quizContainer);
+
+    quizContainer.appendChild(questionPageContainer);
+    questionPageContainer.innerHTML = `
+      <h1>Question ${metadata.currentQuestion + 1} of 5</h1>
+      <form class="quiz-form">
+        <legend class="quiz-question">${currentQuestion}</legend>
+        <div class="row">
+          <input 
+            id="answerA" 
+            class="radio"
+            name="quiz-answer" 
+            type="radio" 
+            value="${answerA}"
+            required
+          >
+          <label for="answerA">${answerA}</label>
+        </div>
+        <br>
+        <div class="row">
+          <input 
+            id="answerB" 
+            class="radio"
+            name="quiz-answer" 
+            type="radio"
+            value="${answerB}"
+            required
+          >
+          <label for="answerB">${answerB}</label>
+        </div>
+        <br>
+        <div class="row">
+          <input 
+            id="answerC" 
+            class="radio"
+            name="quiz-answer" 
+            type="radio" 
+            value="${answerC}"
+            required
+          >
+          <label for="answerC">${answerC}</label>
+        </div>
+        <br>
+        <div class="row">
+          <input 
+            id="answerD" 
+            class="radio"
+            name="quiz-answer" 
+            type="radio" 
+            value="${answerD}"
+            required
+          >
+          <label for="answerD">${answerD}</label>
+        </div>
+        <br>
+        <button type="submit" class="button submit-question">Submit</button>
+      </form>
+    `;
+    const formEl = document.querySelector(".quiz-form");
+    formEl.addEventListener("submit", (e) => handleQuestionSubmit(e));
+  };
+
+  function handleQuizApp() {
+    renderQuizApp();
+    handleQuizStarted();
   };
 
 
-handleQuizStarted()
+  handleQuizApp()
 // I HAVE ZERO CLUE IF THIS WORKS CAUSE NOTHING ELSE DOES
 // let count = 75;
 // const timer = setInterval(function() {
