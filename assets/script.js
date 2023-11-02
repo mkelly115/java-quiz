d1d7e8fb14cbda213abf3ef2f1a427af18c2c10f
michael <mikek222@gmail.com>
2023-11-02 16:42:47 -0400

    final functions added, CSS tweaked, webpage functions but needs to be prettied up

--- assets/script.js
+++ assets/script.js
@@ -54,6 +54,19 @@ function initCountDown() {
     }, 1000);
 };
 
+function generateStartPageTemplate() {
+  const quizContainer = document.querySelector(".quiz-container");
+  const startPageContainer = document.createElement("div");
+  startPageContainer.classList = "flex-centered start-page";
+
+  quizContainer.appendChild(startPageContainer);
+  startPageContainer.innerHTML = `
+    <h1>Coding Quiz Challenge</h1>
+    <button type="button" class="button start-quiz">Start Quiz</button>
+    <button type="button" class="button view-hi-scores">View Hi-Scores</button>
+  `;
+};
+
 function generateQuestionPageTemplate() {
     const quizContainer = document.querySelector(".quiz-container");
     const questionPageContainer = document.createElement("div");
@@ -127,19 +140,6 @@ function generateQuestionPageTemplate() {
     formEl.addEventListener("submit", (e) => handleQuestionSubmit(e));
   };
 
-
-function generateStartPageTemplate() {
-    const quizContainer = document.querySelector(".quiz-container");
-    const startPageContainer = document.createElement("div");
-    startPageContainer.classList = "flex-centered start-page";
-
-    quizContainer.appendChild(startPageContainer);
-    startPageContainer.innerHTML = `
-      <h1>Coding Quiz Challenge</h1>
-      <button type="button" class="button start-quiz">Start Quiz</button>
-      <button type="button" class="button view-hi-scores">View Hi-Scores</button>
-    `;
-};
 function generateAnswerPageTemplate() {
   const quizContainer = document.querySelector(".quiz-container");
   const answerPageContainer = document.createElement("div");
@@ -190,6 +190,33 @@ function generateResultsPageTemplate() {
   restartBtnEl.addEventListener("click", handleResetQuiz);
 };
 
+function generateHiScoresTemplate() {
+  const quizContainer = document.querySelector(".quiz-container");
+  const timerContainer = document.querySelector(".timer");
+
+  
+  emptyPreviousPageContent(quizContainer);
+  emptyPreviousPageContent(timerContainer);
+
+  const hiScoresForm = document.createElement("form");
+  hiScoresForm.classList = "hi-scores-form";
+
+  quizContainer.appendChild(hiScoresForm);
+  hiScoresForm.innerHTML = `
+    <label for="initials">Enter Your Initials:</label>
+    <input id="initials" type="text" required>
+    <button 
+      type="submit" 
+      class="button submit-hiscores margin-top"
+    >
+      Submit
+    </button>
+  `;
+
+  const formEl = document.querySelector(".hi-scores-form");
+  formEl.addEventListener("submit", (e) => handleAddToHiScores(e));
+};
+
 function renderQuizApp() {
     if (!metadata.quizStarted) {
       generateStartPageTemplate();
@@ -200,34 +227,6 @@ function renderQuizApp() {
     } else {
       generateResultsPageTemplate();
     }
-  };
-  
-  
-  function generateHiScoresTemplate() {
-    const quizContainer = document.querySelector(".quiz-container");
-    const timerContainer = document.querySelector(".timer");
-  
-    
-    emptyPreviousPageContent(quizContainer);
-    emptyPreviousPageContent(timerContainer);
-  
-    const hiScoresForm = document.createElement("form");
-    hiScoresForm.classList = "hi-scores-form";
-  
-    quizContainer.appendChild(hiScoresForm);
-    hiScoresForm.innerHTML = `
-      <label for="initials">Enter Your Initials:</label>
-      <input id="initials" type="text" required>
-      <button 
-        type="submit" 
-        class="button submit-hiscores margin-top"
-      >
-        Submit
-      </button>
-    `;
-  
-    const formEl = document.querySelector(".hi-scores-form");
-    formEl.addEventListener("submit", (e) => handleAddToHiScores(e));
   };
 
 function handleQuizStarted() {
@@ -243,76 +242,61 @@ function handleQuizStarted() {
     });
 };
 
-function generateQuestionPageTemplate() {
-    const quizContainer = document.querySelector(".quiz-container");
-    const questionPageContainer = document.createElement("div");
-    questionPageContainer.className = "flex-centered";
-    const currentQuestion = QUIZ_STORE[metadata.currentQuestion].question;
-    const answerA = QUIZ_STORE[metadata.currentQuestion].options[0];
-    const answerB = QUIZ_STORE[metadata.currentQuestion].options[1];
-    const answerC = QUIZ_STORE[metadata.currentQuestion].options[2];
-    const answerD = QUIZ_STORE[metadata.currentQuestion].options[3];
+function handleQuestionSubmit(e) {
+  e.preventDefault();
+  metadata.questionsAnswered++;
+  handleCorrectAnswer();
+  renderQuizApp();
+};
 
-    emptyPreviousPageContent(quizContainer);
+function handleCorrectAnswer() {
+  const userAnswer = document.querySelector(".radio:checked").value;
 
-    quizContainer.appendChild(questionPageContainer);
-    questionPageContainer.innerHTML = `
-      <h1>Question ${metadata.currentQuestion + 1} of 5</h1>
-      <form class="quiz-form">
-        <legend class="quiz-question">${currentQuestion}</legend>
-        <div class="row">
-          <input 
-            id="answerA" 
-            class="radio"
-            name="quiz-answer" 
-            type="radio" 
-            value="${answerA}"
-            required
-          >
-          <label for="answerA">${answerA}</label>
-        </div>
-        <br>
-        <div class="row">
-          <input 
-            id="answerB" 
-            class="radio"
-            name="quiz-answer" 
-            type="radio"
-            value="${answerB}"
-            required
-          >
-          <label for="answerB">${answerB}</label>
-        </div>
-        <br>
-        <div class="row">
-          <input 
-            id="answerC" 
-            class="radio"
-            name="quiz-answer" 
-            type="radio" 
-            value="${answerC}"
-            required
-          >
-          <label for="answerC">${answerC}</label>
-        </div>
-        <br>
-        <div class="row">
-          <input 
-            id="answerD" 
-            class="radio"
-            name="quiz-answer" 
-            type="radio" 
-            value="${answerD}"
-            required
-          >
-          <label for="answerD">${answerD}</label>
-        </div>
-        <br>
-        <button type="submit" class="button submit-question">Submit</button>
-      </form>
-    `;
-    const formEl = document.querySelector(".quiz-form");
-    formEl.addEventListener("submit", (e) => handleQuestionSubmit(e));
+  if (userAnswer && userAnswer !== QUIZ_STORE[metadata.currentQuestion].answer) {
+    metadata.isCorrect = false;
+    metadata.timeLeft -= 5;
+  } else {
+    metadata.isCorrect = true;
+    metadata.score++;
+  }
+};
+
+function handleNextQuestion() {
+  metadata.currentQuestion++;
+  renderQuizApp();
+};
+
+function handleResetQuiz() {
+  const quizContainer = document.querySelector(".quiz-container");
+  const timerContainer = document.querySelector(".timer");
+  metadata.quizStarted = false;
+  metadata.questionsAnswered = 0;
+  metadata.currentQuestion = 0;
+  metadata.score = 0;
+  metadata.timeLeft = 60;
+  emptyPreviousPageContent(quizContainer);
+  emptyPreviousPageContent(timerContainer);
+  handleQuizApp();
+};
+
+function handleAddToHiScores(e) {
+  e.preventDefault();
+  const initials = document.querySelector("input#initials").value;
+  const newHiScore = {
+    initials: initials,
+    score: metadata.score,
+    time: 60 - metadata.timeLeft,
+  };
+
+  const savedScores = localStorage.getItem("hiscores") || "[]"; //get the previously saved hi-scores, or the initial value if empty
+
+  const highScores = [...JSON.parse(savedScores), newHiScore]
+    .sort((a, b) => a.time - b.time) //sort ASC (fastest times first)
+    .slice(0, 10) //take highest 10
+
+  localStorage.setItem("hiscores", JSON.stringify(highScores)); //store the scores
+
+  handleResetQuiz();
 };
 
 function handleViewHiScores() {
